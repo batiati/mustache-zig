6bf5378f404ff6c6ddff48eedb4c3401632da1b0
batiati <rbatiati@gmail.com>
2022-03-20 08:20:06 -0300

    New options proposal

--- src/template.zig
+++ src/template.zig
@@ -315,6 +315,97 @@ pub const TemplateOptions = struct {
     allow_lambdas: bool = true,
 };
 
+///
+/// General options for processing a mustache template
+pub const Options = struct {
+
+    ///
+    /// Template source options
+    source: union(enum) {
+
+        ///
+        /// Loads a template from string
+        String: struct {
+
+            ///
+            /// Use 'false' if the source string is static or lives enough
+            copy_strings: bool = true,
+        },
+
+        ///
+        /// Loads a template from a file or stream
+        Stream: struct {
+
+            ///
+            /// Define the buffer size for reading the stream
+            read_buffer_size: usize = 4 * 1024,
+        },
+    },
+
+    ///
+    /// Template output options
+    output: enum {
+
+        ///
+        /// Parses a template
+        /// Use this option for validation and to store a template for future rendering
+        /// This option speeds up the rendering process when the same template is rendered many times
+        Parse,
+
+        ///
+        /// Parses just enough to render directly, without storing the template.
+        /// This option saves memory.
+        Render,
+    },
+
+    ///
+    /// Those options affect both performance and supported Mustache features.
+    /// Defaults to full-spec compatible.
+    features: struct {
+
+        ///
+        /// Default delimiters are '{{' and '}}'
+        /// Allow redefining new delimiters such as '[' and ']'
+        default_delimiters: Delimiters = .{},
+
+        ///
+        /// Allows redefining the delimiters through the tags '{{=' and '=}}'
+        /// Disabling this option speeds up the parsing process.
+        /// If disabled, any occurrence of '{{=' will result in a parse error
+        allow_redefining_delimiters: bool = true,
+
+        ///
+        /// Preserve line breaks and indentations.
+        /// This option is useful when rendering documents sensible to spaces such as `yaml` for example.
+        /// Disabling this option speeds up the parsing process.
+        /// Examples:
+        /// [Line breaks](https://github.com/mustache/spec/blob/b2aeb3c283de931a7004b5f7a2cb394b89382369/specs/comments.yml#L38)
+        /// [Indentation](https://github.com/mustache/spec/blob/b2aeb3c283de931a7004b5f7a2cb394b89382369/specs/partials.yml#L82)
+        preseve_line_breaks_and_indentation: bool = true,
+
+        ///
+        /// Lambda expansion support
+        lambdas: union(enum) {
+
+            ///
+            /// Use this option if your data source does not implement lambda functions
+            /// Disabling lambda support saves memory and speeds up the parsing process
+            Disabled,
+
+            ///
+            /// Use this option to support lambda functions in your data sources
+            Enabled: struct {
+
+                ///
+                /// Lambdas can expand to new tags, including another lambda
+                /// Defines the max recursion depth to avoid infinite recursion when evaluating lambdas
+                /// A recursive lambda will interpolate as an empty string, without erros
+                max_recursion: comptime_int = 100,
+            },
+        } = .{ .Enabled = .{} },
+    } = .{},
+};
+
 pub fn TemplateLoader(comptime options: TemplateOptions) type {
     const FileCachedParser = parsing.Parser(.{
         .source = .File,
